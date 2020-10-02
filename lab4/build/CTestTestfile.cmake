81b993c5677a6d23adc801badeb3e7a3e70da26b
Sahil Saxena <sahilsaxena18@berkeley.edu>
2020-10-02 09:48:32 -0700

    lab4 starter

---
+++ lab4/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sahilsaxena/ros_workspaces/lab4/src
+# Build directory: /home/sahilsaxena/ros_workspaces/lab4/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(lab4_starter)
---
+++ lab4/build/lab4_starter/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sahilsaxena/ros_workspaces/lab4/src/lab4_starter
+# Build directory: /home/sahilsaxena/ros_workspaces/lab4/build/lab4_starter
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
