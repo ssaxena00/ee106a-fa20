97a39db70a2966d17602152252d2ebea6f25ae97
Sahil Saxena <sahilsaxena18@berkeley.edu>
2020-10-28 16:48:04 -0700

    lab6 starter stuff

---
+++ lab6/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/sahilsaxena/ros_workspaces/lab6/src
+# Build directory: /home/sahilsaxena/ros_workspaces/lab6/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(ros_numpy)
+subdirs(ddynamic_reconfigure)
+subdirs(segmentation)
