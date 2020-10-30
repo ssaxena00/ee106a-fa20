86a9379466522cb49593196ced33c8253ef25aa9
Sahil Saxena <sahilsaxena18@berkeley.edu>
2020-10-30 12:00:57 -0700

    finished 7.1

---
+++ lab7/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/sahilsaxena/ros_workspaces/lab7/src
+# Build directory: /home/sahilsaxena/ros_workspaces/lab7/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(ros_numpy)
+subdirs(stereo_pointcloud)
---
+++ lab7/build/stereo_pointcloud/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sahilsaxena/ros_workspaces/lab7/src/stereo_pointcloud
+# Build directory: /home/sahilsaxena/ros_workspaces/lab7/build/stereo_pointcloud
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
